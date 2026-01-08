03270980bda8316f96bde3b72b70b1c9d89e21bd
emersonsilvagit <emerson_asilva@hotmail.com>
2026-01-08 09:01:05 -0300

    Add dotenv configuration for environment variables
    Add dotenv configuration for environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as dotenv from 'dotenv';
 
+// TEST
 dotenv.config();
 
 // ===== Build-time safe Git info =====
